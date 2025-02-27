734174d8485fe56b557d5e343a3d10243c5b6fff
StalkerPro <andreit007@mail.ru>
2025-02-27 21:09:56 +0700

    not bad))

---
+++ main.js
@@ -0,0 +1,143 @@
+// Офисный стиль
+function createOfficeChair() {
+	return {
+		type: 'Chair',
+		style: 'Office',
+		image:
+			'https://taurus-m.ru/upload/resize_cache/webp/iblock/4aa/vssauqj6zya2qqltp0cxxtp3d3i36vqd.webp',
+	}
+}
+
+function createOfficeTable() {
+	return {
+		type: 'Table',
+		style: 'Office',
+		image:
+			'https://ouch-cdn2.icons8.com/dcwWnWUr52hXQHMZ_x3S7_JU4C6bBdV56sH4X6Ufao8/rs:fit:569:456/extend:false/wm:1:re:0:0:0.8/wmid:ouch2/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTM4/LzQwOGY0ZWJmLTc0/YmItNDljYy05YmQ1/LTIzNjMyOTgwNjc3/Yi5zdmc.png',
+	}
+}
+
+function createOfficeSofa() {
+	return {
+		type: 'Sofa',
+		style: 'Office',
+		image:
+			'https://cdn.domdivanov24.com/files/imgs/ig1111919/cnkrzqlg-590x430.jpg',
+	}
+}
+
+// Винтажный стиль
+function createVintageChair() {
+	return {
+		type: 'Chair',
+		style: 'Vintage',
+		image: 'https://abali.ru/wp-content/uploads/2014/05/stul.jpg',
+	}
+}
+
+function createVintageTable() {
+	return {
+		type: 'Table',
+		style: 'Vintage',
+		image:
+			'https://previews.123rf.com/images/castecodesign/castecodesign1607/castecodesign160700157/59308014-%D0%B1%D0%B0%D1%80%D0%BE%D0%BA%D0%BA%D0%BE-%D0%BA%D0%BE%D1%80%D0%BE%D0%BB%D0%B5%D0%B2%D1%81%D0%BA%D0%B8%D0%B9-%D1%81%D1%82%D0%BE%D0%BB-%D1%81-%D1%80%D0%BE%D1%81%D0%BA%D0%BE%D1%88%D0%BD%D1%8B%D0%BC-%D0%BE%D1%80%D0%BD%D0%B0%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%BC-%D0%B2-%D1%87%D0%B5%D1%80%D0%BD%D0%BE%D0%BC-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80-%D0%BC%D0%B5%D0%B1%D0%B5%D0%BB%D1%8C-%D1%81-%D0%B3%D1%80%D0%B0%D0%B2%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC.jpg',
+	}
+}
+
+function createVintageSofa() {
+	return {
+		type: 'Sofa',
+		style: 'Vintage',
+		image:
+			'https://previews.123rf.com/images/castecodesign/castecodesign1701/castecodesign170100293/70820909-%D0%B2%D0%B8%D0%BD%D1%82%D0%B0%D0%B6%D0%BD%D1%8B%D0%B9-%D0%B1%D0%B0%D1%80%D0%BE%D0%BA%D0%BA%D0%BE-%D0%B8%D0%BC%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80%D1%81%D0%BA%D0%B8%D0%B9-%D0%B4%D0%B8%D0%B2%D0%B0%D0%BD-%D1%84%D1%80%D0%B0%D0%BD%D1%86%D1%83%D0%B7%D1%81%D0%BA%D0%B0%D1%8F-%D1%80%D0%BE%D1%81%D0%BA%D0%BE%D1%88%D0%BD%D0%B0%D1%8F-%D0%B1%D0%BE%D0%B3%D0%B0%D1%82%D0%B0%D1%8F-%D0%B4%D0%B5%D0%BA%D0%BE%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%B0%D1%8F-%D0%BC%D0%B5%D0%B1%D0%B5%D0%BB%D1%8C.jpg',
+	}
+}
+
+// Классический стиль
+function createClassicChair() {
+	return {
+		type: 'Chair',
+		style: 'Classic',
+		image:
+			'https://previews.123rf.com/images/afendikoff/afendikoff1711/afendikoff171100035/90426634-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D1%8F%D0%BD%D0%BD%D1%8B%D0%B9-%D1%81%D1%82%D1%83%D0%BB-%D0%B1%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE-%D1%86%D0%B2%D0%B5%D1%82%D0%B0-%D1%81%D0%BE-%D1%81%D0%BF%D0%B8%D0%BD%D0%BA%D0%BE%D0%B9-%D0%B8%D0%B7%D0%BE%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC-%D1%84%D0%BE%D0%BD%D0%B5-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F.jpg',
+	}
+}
+
+function createClassicTable() {
+	return {
+		type: 'Table',
+		style: 'Classic',
+		image:
+			'https://previews.123rf.com/images/dvarg/dvarg1308/dvarg130800127/21760393-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D1%80%D1%83%D0%B3%D0%BB%D1%8B%D0%B9-%D1%81%D1%82%D0%BE%D0%BB-%D1%81-%D0%B4%D0%BB%D0%B8%D0%BD%D0%BD%D0%BE%D0%B9-%D0%B1%D0%B5%D0%BB%D0%BE%D0%B9-%D1%82%D0%BA%D0%B0%D0%BD%D1%8C%D1%8E-%D0%B8%D0%B7%D0%BE%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg',
+	}
+}
+
+function createClassicSofa() {
+	return {
+		type: 'Sofa',
+		style: 'Classic',
+		image:
+			'https://pmv173.ru/upload/iblock/724/8rlfb72ld43b4fobtbdzlmcugzhss9qs.jpg',
+	}
+}
+
+function choiceOffice() {
+	const officeChair = createOfficeChair()
+	const officeTable = createOfficeTable()
+	const officeSofa = createOfficeSofa()
+
+	return [officeChair, officeSofa, officeTable]
+}
+
+function choiceClassic() {
+	const classicChair = createClassicChair()
+	const classicTable = createClassicTable()
+	const classicSofa = createClassicSofa()
+
+	return [classicChair, classicTable, classicSofa]
+}
+
+function choiceVintage() {
+	const vintageChair = createVintageChair()
+	const vintageTable = createVintageTable()
+	const vintageSofa = createVintageSofa()
+
+	return [vintageChair, vintageTable, vintageSofa]
+}
+
+// console.log(choiceOffice()) // { type: 'Chair', style: 'Office' }
+// console.log(vintageTable) // { type: 'Table', style: 'Vintage' }
+// console.log(classicSofa) // { type: 'Sofa', style: 'Classic' }
+
+// Функция для отображения предметов на странице
+function displayFurniture(items) {
+	const container = document.getElementById('furniture-container')
+	container.innerHTML = '' // Очищаю контейнер перед добавлением новых элементов
+
+	items.forEach(item => {
+		const itemElement = document.createElement('div')
+		itemElement.className = 'furniture-item'
+		itemElement.innerHTML = `
+			<h2>${item.type} (${item.style})</h2>
+			<img src="${item.image}" alt="${item.type}">
+		`
+		container.appendChild(itemElement)
+	})
+}
+
+// Обработчик события для кнопки "Classic"
+document.querySelector('.btn--1').addEventListener('click', () => {
+	const classicItems = choiceClassic()
+	displayFurniture(classicItems)
+})
+
+// Аналогично можно добавить обработчики для других кнопок
+document.querySelector('.btn--2').addEventListener('click', () => {
+	const officeItems = choiceOffice()
+	displayFurniture(officeItems)
+})
+
+document.querySelector('.btn--3').addEventListener('click', () => {
+	const vintageItems = choiceVintage()
+	displayFurniture(vintageItems)
+})
